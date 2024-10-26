b361773d9149bcd906ccf18c1a2f1143ea6febdc
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-10-26 16:46:01 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Header } from './components';
 
 const App = () => {
   return (
-    <div >App</div>
+    <div className='w-screen h-auto flex flex-col'><Header /></div>
   )
 }
 
