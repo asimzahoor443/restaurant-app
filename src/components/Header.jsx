1568cad2c9fe0f7a01d032b17274c38d9aba5a06
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-11-01 08:32:30 +0500

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
-import Logo from '../img/logo.png';
-import { IoMdBasket } from 'react-icons/io';
-import Avatar from '../img/avatar.png';
+import React, { useState } from 'react';
+import { IoMdBasket, IoMdAdd, IoMdLogOut } from 'react-icons/io';
 import { motion } from 'framer-motion';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { app } from '../firebase.config';
+import Logo from '../img/logo.png';
+import Avatar from '../img/avatar.png';
 import { Link } from 'react-router-dom';
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
+import { type } from '@testing-library/user-event/dist/type';
 
 const Header = () => {
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const [{ user }, dispatch] = useStateValue();
+
+  const [isMenu, setIsMenu] = useState(false);
+
+  const login = async () => {
+    if (!user) {
+      const {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        USER: providerData[0],
+      });
+
+      localStorage.setItem('user', JSON.stringify(providerData[0]));
+    } else {
+      setIsMenu(!isMenu);
+    }
+  };
+
+  const logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
+  };
   return (
-    <header className=" fixed  z-50 w-screen p-6 px-16">
+    <header className=" fixed  z-50 w-screen p-3 px-4 md:p-6 md:px-16">
       {/* Desktop and Tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to={'/'} className="flex items-center gap-2">
@@ -15,7 +52,12 @@ const Header = () => {
           <p className="text-headingColor text-xl font-bold">City</p>
         </Link>
         <div className="flex items-center gap-8">
-          <ul className="flex items-center gap-8">
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className="flex items-center gap-8"
+          >
             <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
               Home
             </li>
@@ -28,26 +70,100 @@ const Header = () => {
             <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
               Service
             </li>
-          </ul>
+          </motion.ul>
           <div className="relative flex items-center justify-center">
             <IoMdBasket className="text-textColor text-2xl cursor-pointer" />
             <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-xs text-white font-semibold">2</p>
             </div>
           </div>
-          <motion.img
-            whileTap={{ scale: 0.6 }}
-            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"
-            src={Avatar}
-            alt="userprofile"
-          />
+          <div className="relative">
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+              src={user ? user.photoURL : Avatar}
+              alt="userprofile"
+              onClick={login}
+            />
+            {isMenu && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.6 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.6 }}
+                className="w-40 bg-gray-50 shadow-xl rounded-lg  flex flex-col absolute top-12 right-0 "
+              >
+                {user && user.email === 'asimzahoor443@gmail.com' && (
+                  <Link to={'/createItem'}>
+                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                      New Item <IoMdAdd />
+                    </p>
+                  </Link>
+                )}
+                <p
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                  onClick={logout}
+                >
+                  Logout <IoMdLogOut />
+                </p>
+              </motion.div>
+            )}
+          </div>
         </div>
       </div>
 
       {/* Mobile View */}
-      <div className="flex md:hidden w-full h-full  p-4"></div>
+      <div className="flex items-center justify-between md:hidden w-full h-full  p-4">
+        <Link to={'/'} className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="logo" />
+          <p className="text-headingColor text-xl font-bold">City</p>
+        </Link>
+        <div className="relative">
+          <motion.img
+            whileTap={{ scale: 0.6 }}
+            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+            src={user ? user.photoURL : Avatar}
+            alt="userprofile"
+            onClick={login}
+          />
+          {isMenu && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              className="w-40 bg-gray-50 shadow-xl rounded-lg  flex flex-col absolute top-12 right-0 "
+            >
+              {user && user.email === 'asimzahoor443@gmail.com' && (
+                <Link to={'/createItem'}>
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    New Item <IoMdAdd />
+                  </p>
+                </Link>
+              )}
+              <ul className="flex flex-col">
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100  px-4 py-2">
+                  Home
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100  px-4 py-2">
+                  Menu
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100  px-4 py-2">
+                  About Us
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100  px-4 py-2">
+                  Service
+                </li>
+              </ul>
+              <p
+                className="m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base"
+                onClick={logout}
+              >
+                Logout <IoMdLogOut />
+              </p>
+            </motion.div>
+          )}
+        </div>
+      </div>
     </header>
   );
 };
-
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import { StateProvider } from './context/StateProvider';
+import { initialState } from './context/initialState';
+import reducer from './context/reducer';
 
-ReactDOM.render(
-    <Router >
-<App />
-    </Router>
-, 
-document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </Router>
 );
