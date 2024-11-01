2e8f3fc2ebf2770237cb988d6402c93cf26e5e40
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-11-01 09:33:51 +0500

    Updates

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Delivery from '../img/delivery.png';
 
-const MainContainer = () => {
+const HomeContainer = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
@@ -41,4 +41,4 @@ const MainContainer = () => {
   );
 };
 
-export default MainContainer;
+export default HomeContainer;
