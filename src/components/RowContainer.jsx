44fb2fe70252e06855c11b04c2b791a5f3f1b3c4
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-11-17 08:11:46 +0500

    Updates

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,42 +1,17 @@
-import React, { useEffect, useRef, useState } from "react";
-import { MdShoppingBasket } from "react-icons/md";
-import { motion } from "framer-motion";
-import NotFound from "../img/NotFound.svg";
-import { useStateValue } from "../context/StateProvider";
-import { actionType } from "../context/reducer";
+import React from 'react';
+import { MdShoppingBasket } from 'react-icons/md';
+import { motion } from 'framer-motion';
 
-const RowContainer = ({ flag, data, scrollValue }) => {
-  const rowContainer = useRef();
-
-  const [items, setItems] = useState([]);
-
-  const [{ cartItems }, dispatch] = useStateValue();
-
-  const addtocart = () => {
-    dispatch({
-      type: actionType.SET_CARTITEMS,
-      cartItems: items,
-    });
-  };
-
-  useEffect(() => {
-    rowContainer.current.scrollLeft += scrollValue;
-  }, [scrollValue]);
-
-  useEffect(() => {
-    addtocart();
-  }, [items]);
-
+const RowContainer = ({ flag, data }) => {
   return (
     <div
-      ref={rowContainer}
       className={`w-full flex items-center gap-3  my-12 scroll-smooth  ${
         flag
-          ? "overflow-x-scroll scrollbar-none"
-          : "overflow-x-hidden flex-wrap justify-center"
+          ? 'overflow-x-scroll scrollbar-none'
+          : 'overflow-x-hidden flex-wrap'
       }`}
     >
-      {data && data.length > 0 ? (
+      {data &&
         data.map((item) => (
           <div
             key={item?.id}
@@ -56,7 +31,6 @@ const RowContainer = ({ flag, data, scrollValue }) => {
               <motion.div
                 whileTap={{ scale: 0.75 }}
                 className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md -mt-8"
-                onClick={() => setItems([...cartItems, item])}
               >
                 <MdShoppingBasket className="text-white" />
               </motion.div>
@@ -76,15 +50,7 @@ const RowContainer = ({ flag, data, scrollValue }) => {
               </div>
             </div>
           </div>
-        ))
-      ) : (
-        <div className="w-full flex flex-col items-center justify-center">
-          <img src={NotFound} className="h-340" />
-          <p className="text-xl text-headingColor font-semibold my-2">
-            Items Not Available
-          </p>
-        </div>
-      )}
+        ))}
     </div>
   );
 };
