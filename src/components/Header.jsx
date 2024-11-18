702d77222466c70c51b83057a42fa3a8f561457b
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-11-18 12:52:52 +0500

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
-  const [{ user }, dispatch] = useStateValue();
+  const [{ user, cartShow }, dispatch] = useStateValue();
 
   const [isMenu, setIsMenu] = useState(false);
 
@@ -41,6 +41,13 @@ const Header = () => {
     dispatch({
       type: actionType.SET_USER,
       user: null,
+    });
+  };
+
+  const showCart = () => {
+    dispatch({
+      type: actionType.SET_CART_SHOW,
+      cartShow: !cartShow,
     });
   };
   return (
@@ -71,7 +78,10 @@ const Header = () => {
               Service
             </li>
           </motion.ul>
-          <div className="relative flex items-center justify-center">
+          <div
+            className="relative flex items-center justify-center"
+            onClick={showCart}
+          >
             <IoMdBasket className="text-textColor text-2xl cursor-pointer" />
             <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
               <p className="text-xs text-white font-semibold">2</p>
@@ -116,7 +126,10 @@ const Header = () => {
 
       {/* Mobile View */}
       <div className="flex items-center justify-between md:hidden w-full h-full  p-4">
-        <div className="relative flex items-center justify-center">
+        <div
+          className="relative flex items-center justify-center"
+          onClick={showCart}
+        >
           <IoMdBasket className="text-textColor text-2xl cursor-pointer" />
           <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
             <p className="text-xs text-white font-semibold">2</p>
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -8,13 +8,11 @@ import MenuContainer from './MenuContainer';
 import CartContainer from './CartContainer';
 
 const MainContainer = () => {
-  const [{ foodItems }, dispatch] = useStateValue();
+  const [{ foodItems, cartShow }, dispatch] = useStateValue();
 
   const [scrollValue, setScrollValue] = useState(0);
 
-  useEffect(() => {
-    console.log('Food Items', foodItems);
-  }, [scrollValue]);
+  useEffect(() => {}, [scrollValue, cartShow]);
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center ">
       <HomeContainer />
@@ -49,7 +47,8 @@ const MainContainer = () => {
         />
       </section>
       <MenuContainer />
-      <CartContainer />
+
+      {cartShow && <CartContainer />}
     </div>
   );
 };
